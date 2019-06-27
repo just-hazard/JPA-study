1d0fd00044413dfa151b1e000245dc8c46db5f13
Seop <edenhazard5870@gmail.com>
2019-06-27 19:43:23 +0900

    4번째 영상 관계 맵핑까지 실습

--- src/main/java/com/example/jpa/Account.java
+++ src/main/java/com/example/jpa/Account.java
@@ -1,20 +1,44 @@
 package com.example.jpa;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
-@Data
+@Getter
+@Setter
 public class Account {
 
     @Id
     @GeneratedValue
     private Long id;
 
+    @Column
     private String username;
 
     private String password;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created_time = new Date();
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "owner")
+    private Set<Study> studies = new HashSet<>();
+
+    public void addStudy(Study study) {
+        this.getStudies().add(study);
+        study.setOwner(this);
+    }
+
+    public void removeStudy(Study study) {
+        this.getStudies().remove(study);
+        study.setOwner(null);
+    }
 }
--- src/main/java/com/example/jpa/JpaRunner.java
+++ src/main/java/com/example/jpa/JpaRunner.java
@@ -1,5 +1,6 @@
 package com.example.jpa;
 
+import org.hibernate.Session;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -21,6 +22,17 @@ public class JpaRunner implements ApplicationRunner {
         account.setUsername("seop");
         account.setPassword("1234");
 
-        entityManager.persist(account);
+        Study study = new Study();
+        study.setName("Sring Data JPA");
+
+        account.addStudy(study);
+
+//        account.getStudies().add(study);
+//        study.setOwner(account);
+
+        Session session = entityManager.unwrap(Session.class);
+
+        session.save(account);
+        session.save(study);
     }
 }
