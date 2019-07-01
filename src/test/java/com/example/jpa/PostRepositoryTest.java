517a5dc3f9d94ddd5bb6936073fb44a17bf3632e
Seop <edenhazard5870@gmail.com>
2019-07-01 20:27:50 +0900

    스프링 데이터 Common 학습중 11분째

---
+++ src/test/java/com/example/jpa/PostRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.example.jpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class PostRepositoryTest {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Test
+    @Rollback(false)
+    public void crudRepository() {
+        // Given
+        Post post = new Post();
+        post.setTitle("test");
+        assertThat(post.getId()).isNull();
+
+        // When
+        Post newPost = postRepository.save(post);
+
+        // Then
+        assertThat(newPost.getId()).isNotNull();
+    }
+
+}
